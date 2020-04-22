a13d170f23afa8353c33d229a9cfbcc1ba1d5498
fred <frederic.boisguerin@edulib.fr>
2020-04-22 10:59:39 +0200

    Add skeleton

---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'src'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rspec'
---
+++ spec/sudoku_spec.rb
@@ -0,0 +1,6 @@
+require_relative 'spec_helper'
+require 'sudoku'
+
+describe Sudoku do
+
+end
