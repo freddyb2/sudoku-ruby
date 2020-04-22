48dd4018b1d5edd40910a9f390b583ce0d3b30b8
fred <frederic.boisguerin@edulib.fr>
2020-04-22 12:18:05 +0200

    Solve square when one char missing

--- src/sudoku.rb
+++ src/sudoku.rb
@@ -9,10 +9,26 @@ class Sudoku
 
   private
 
-
   def solve_lines
     self.solution = solution.map(&method(:complete_line))
     self.solution = reverse_grid(reverse_grid(solution).map(&method(:complete_line)))
+    self.solution = reverse_squares(reverse_squares(solution).map(&method(:complete_line)))
+  end
+
+  def reverse_squares(grid)
+    (0..(SQUARES_SIDE - 1)).map do |square_line_index|
+      (0..(SQUARES_SIDE - 1)).map do |square_column_index|
+        line_min = square_line_index * SQUARES_SIDE
+        line_max = line_min + (SQUARES_SIDE - 1)
+        column_min = square_column_index * SQUARES_SIDE
+        column_max = column_min + (SQUARES_SIDE - 1)
+
+        line_select = grid.select.with_index { |_, index| (line_min..line_max).to_a.include? index }
+        reverse_grid = reverse_grid(line_select)
+        column_select = reverse_grid.select.with_index { |_, index| (column_min..column_max).to_a.include? index }
+        reverse_grid(column_select).join
+      end
+    end.flatten
   end
 
   def reverse_grid grid
@@ -22,6 +38,7 @@ class Sudoku
   CHAR_TO_COMPLETE = "x".freeze
   CHAR_TO_IGNORE = ".".freeze
   NUMBERS = (1..9).to_a.freeze
+  SQUARES_SIDE = Math.sqrt(NUMBERS.count).to_i
   CHARS_AUTHORIZED = NUMBERS.map(&:to_s).freeze
 
   def complete_line line
