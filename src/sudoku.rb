e7f4f85db786c8791b542e4af8f4dbf57916b079
fred <frederic.boisguerin@edulib.fr>
2020-04-25 16:54:23 +0200

    Solve difficult grids

--- src/sudoku.rb
+++ src/sudoku.rb
@@ -16,28 +16,46 @@ class Sudoku
   private
 
   def check_chars! chars
-    chars_except_ignored = (chars - [CHAR_TO_COMPLETE])
-    raise "ERROR" if chars_except_ignored.count != chars_except_ignored.uniq.count
+    raise "NOT COMPLETE" unless (CHARS_AUTHORIZED - chars).empty? && chars.count == CHARS_AUTHORIZED.count
   end
 
   def check_solution!
-    SIDE_INDEXES.each { |index| check_chars! chars_in_line(index) }
-    SIDE_INDEXES.each { |index| check_chars! chars_in_column(index) }
-    (0..(GRID_POWER-1)).each do |v_index|
-      (0..(GRID_POWER-1)).each do |h_index|
-        check_chars! chars_in_square(v_index * GRID_POWER, h_index * GRID_POWER)
-      end
-    end
+    CELLS_INDEXES.each { |index| check_chars! chars_in_line(index) }
+    CELLS_INDEXES.each { |index| check_chars! chars_in_column(index) }
+    SQUARES_INDEXES.each { |x| SQUARES_INDEXES.each { |y| check_chars! chars_in_square(x * GRID_POWER, y * GRID_POWER) } }
+  end
+
+  def possibilities(line_index, column_index)
+    cell = cell(line_index, column_index)
+    return nil if cell != CHAR_TO_COMPLETE
+    CHARS_AUTHORIZED - chars_in_line(line_index) - chars_in_column(column_index) - chars_in_square(line_index, column_index)
+  end
+
+  def possibilities_in_square(cell_line_index, cell_column_index)
+    square_line_index = cell_line_index / GRID_POWER
+    square_column_index = cell_column_index / GRID_POWER
+    line_min = square_line_index * GRID_POWER
+    line_max = line_min + (GRID_POWER - 1)
+    column_min = square_column_index * GRID_POWER
+    column_max = column_min + (GRID_POWER - 1)
+    (line_min..line_max).map { |line_index| (column_min..column_max).map { |column_index| (cell_column_index == line_index && cell_column_index == column_index) ? nil : possibilities(line_index, column_index) } }.compact.flatten
   end
 
 
   def solve_lines
-    SIDE_INDEXES.each do |line_index|
-      SIDE_INDEXES.each do |column_index|
-        cell = cell(line_index, column_index)
-        next unless cell == CHAR_TO_COMPLETE
-        possibilities = CHARS_AUTHORIZED - chars_in_line(line_index) - chars_in_column(column_index) - chars_in_square(line_index, column_index)
-        write_cell(line_index, column_index, possibilities.first) if possibilities.count == 1
+    CELLS_INDEXES.each do |line_index|
+      CELLS_INDEXES.each do |column_index|
+        possibilities = possibilities(line_index, column_index)
+        next unless possibilities
+        possibilities_line = possibilities - CELLS_INDEXES.select { |y| y != column_index }.map { |y| possibilities(line_index, y) }.flatten
+        possibilities_column = possibilities - CELLS_INDEXES.select { |x| x != line_index }.map { |x| possibilities(x, column_index) }.flatten
+        possibilities_square = possibilities - possibilities_in_square(line_index, column_index)
+        cell_solutions = [possibilities, possibilities_line, possibilities_column, possibilities_square]
+                           .select { |p| p.count == 1 }
+                           .flatten
+                           .uniq
+
+        write_cell(line_index, column_index, cell_solutions.first) if cell_solutions.count == 1
       end
     end
   end
@@ -49,11 +67,11 @@ class Sudoku
   end
 
   def chars_in_line(line_index)
-    SIDE_INDEXES.map { |column_index| cell(line_index, column_index) }
+    CELLS_INDEXES.map { |column_index| cell(line_index, column_index) }
   end
 
   def chars_in_column(column_index)
-    SIDE_INDEXES.map { |line_index| cell(line_index, column_index) }
+    CELLS_INDEXES.map { |line_index| cell(line_index, column_index) }
   end
 
   def cell(line_index, column_index)
@@ -72,7 +90,8 @@ class Sudoku
 
   CHAR_TO_COMPLETE = "x".freeze
   GRID_POWER = 3
-  SIDE_INDEXES = (0..(GRID_POWER ** 2 - 1)).to_a.freeze
+  CELLS_INDEXES = (0..(GRID_POWER ** 2 - 1)).to_a.freeze
+  SQUARES_INDEXES = (0..(GRID_POWER - 1))
   CHARS_AUTHORIZED = (1..(GRID_POWER ** 2)).map(&:to_s).freeze
 
   def complete_line line
